fbb7a90a1c1c9f47afba0739a24143ba6647b879
Daria <dasham94@yandex.ru>
2015-10-27 15:58:13 +0300

    заготовка для принтера

---
+++ src/tree/CPrinter.cpp
@@ -0,0 +1,25 @@
+//
+// Created by daria on 27.10.15.
+//
+
+#include "CPrinter.h"
+
+void CPrinter::Visit(const CAssignmentExpression *assignmentExpression) { }
+void CPrinter::Visit(const CBasicType *basicType) { }
+void CPrinter::Visit(const CBinaryExpression *binaryExpression) { }
+void CPrinter::Visit(const CClassType *classType) { }
+void CPrinter::Visit(const CConstant *constant) { }
+void CPrinter::Visit(const CExpressionList *expressionList) { }
+void CPrinter::Visit(const CIdentifier *identifier) { }
+void CPrinter::Visit(const CIfStatement *ifStatement) { }
+void CPrinter::Visit(const CIntArrayAssignmentExpression *arrayAssignmentExpression) { }
+void CPrinter::Visit(const CIntArrayNewExpression *intArrayNewExpression) { }
+void CPrinter::Visit(const CInvocation *invocation) { }
+void CPrinter::Visit(const CLengthExpression *lengthExpression) { }
+void CPrinter::Visit(const CNewExpression *newExpression) { }
+void CPrinter::Visit(const CNotExpression *notExpression) { }
+void CPrinter::Visit(const CPrintStatement *printStatement) { }
+void CPrinter::Visit(const CStatementList *statementList) { }
+void CPrinter::Visit(const CVarDeclaration *varDeclaration) { }
+void CPrinter::Visit(const CVarDeclarationList *varDeclarationList) { }
+void CPrinter::Visit(const CWhileStatement *whileStatement) { }
